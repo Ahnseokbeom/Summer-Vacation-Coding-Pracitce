efb7f21ed9f89c265834b84e0d07dea88e4660c2
seobkeom <ajm153123@naver.com>
2022-07-21 13:21:04 +0900

    Coding Practice 3Week final commit

--- SummerCoding/src/PRO_2_OpenChat.java
+++ SummerCoding/src/PRO_2_OpenChat.java
@@ -6,22 +6,25 @@ public class PRO_2_OpenChat {
 	    public String[] solution(String[] record) {
 	    	ArrayList<String> list = new ArrayList<String>();
 	    	HashMap<String, String> name = new HashMap<>();
-	    	for(int i = 0;i<record.length;i++) { // arr배열을 공백 기준으로 구분
+	    	for(int i = 0;i<record.length;i++) {
 	    		String[] arr = record[i].split(" ");
-	    		if(arr[0].equals("Enter")) { // 0번째 인덱스가 enter이면(입장)
+	    		// 입장
+	    		if(arr[0].equals("Enter")) {
 	    			name.put(arr[1], arr[2]); // id와 username을 map에 넣는다.
-	    			list.add(arr[1]+"님이 들어왔습니다."); // list의 id를 넣은 결과 추가
-	    		}else if(arr[0].equals("Leave")) { // 0번째 인덱스가 leave이면(퇴장)
-	    			list.add(arr[1]+"님이 나갔습니다."); // list의 id를 넣고 결과 추가
-	    		}else if(arr[0].equals("Change")) { // 0번째 인덱스가 change이면(바꿈)
+	    			list.add(arr[1]+"님이 들어왔습니다.");
+	    			// 퇴장
+	    		}else if(arr[0].equals("Leave")) {
+	    			list.add(arr[1]+"님이 나갔습니다.");
+	    			// 변경
+	    		}else if(arr[0].equals("Change")) {
 	    			name.replace(arr[1], arr[2]); // id와 username의 값을 바꿔준다.
 	    		}
 	    	}
 	    	String[] answer = new String[list.size()];
 	    	for(int i = 0;i<answer.length;i++) {
-	    		int idx = list.get(i).indexOf("님");
-	    		String str = list.get(i).substring(0,idx);
-	    		answer[i] = name.get(str)+list.get(i).substring(idx);
+	    		int Nidx = list.get(i).indexOf("님");
+	    		String str = list.get(i).substring(0,Nidx);
+	    		answer[i] = name.get(str)+list.get(i).substring(Nidx);
 	    	}
 
 	        return answer;
---
+++ SummerCoding/src/PRO_2_StringZip.java
@@ -0,0 +1,42 @@
+
+public class PRO_2_StringZip {
+	public static void main(String[] args) {
+		String s = "ababcdcdababcdcd";
+//		String s = "abcabcdede";
+//		String s = "abcabcabcabcdededededede";
+//		String s = "xababcdcdababcdcd";
+//		String s = "aabbaccc";
+		int answer = 0;
+		if(s.length()==1) answer = 1;
+		for (int i = 1; i < s.length() / 2 + 1; i++) {
+			 // pattern
+            String pattern = s.substring(0, i);
+            // 압축 끝난 문자열
+            String end = "";
+            // 현재 비교되는 대상
+            String curr = "";
+            // 반복 횟수
+            int count = 1;
+            for (int j = i; j < s.length(); j += i) {
+                if (j + i > s.length()) end += s.substring(j, s.length());
+                else {
+                    curr = s.substring(j, j + i);
+                    if (curr.equals(pattern)) {
+                        count++;
+                    } else {
+                        end += ((count == 1 ? "" : count + "") + pattern);
+                        pattern = s.substring(j, j + i);
+                        count = 1;
+                    }
+                }
+            }
+            String result = (count == 1 ? "" : count + "") + pattern + end;
+            if (answer == 0) {
+                answer = result.length();
+            }
+            if(result.length() < answer) answer = result.length();
+		}
+		System.out.println(answer);
+	}
+
+}

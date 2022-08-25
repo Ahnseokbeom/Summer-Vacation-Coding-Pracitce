f2826de129a482e2f43d9a989cc211226131685b
seobkeom <ajm153123@naver.com>
2022-08-25 20:02:05 +0900

    AtoB commit(재귀연습)

---
+++ SummerCoding/src/Week8/AtoB.java
@@ -0,0 +1,20 @@
+package Week8;
+
+public class AtoB {
+	static int sum1 = 0;
+	public static void main(String[] args) {
+		int A = 1;
+		int B = 10;
+		sum(A,B);
+	}
+	public static int sum(int A, int B) {
+		sum1+=A;
+		A++;
+		if(A==10) {
+			System.out.println("합계 : "+sum1);
+			return 0;
+		}
+		return sum(A,B);
+	}
+
+}
